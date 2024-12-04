ef0e90970920c066951f8892c0dad32acacd8ef6
NaWi <nawi@localhost.localdomain>
2024-12-04 20:11:50 +0100

    name change temp

--- research/ONNXDemoApp/ugabuga/C-verify-size.py
+++
@@ -1,4 +0,0 @@
-import os
-quantized_model_path = "distilbert_base_uncased_quantized.onnx"
-size_in_mb = os.path.getsize(quantized_model_path) / (1024 * 1024)
-print(f"Quantized model size: {size_in_mb:.2f} MB")
