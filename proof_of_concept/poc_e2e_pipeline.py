2e881939643939b34401f5bdcde2c0819d6b25a4
Gustaw Blachowski <gustaw.blachowski@gmail.com>
2024-12-12 13:53:49 +0100

    Finish e2e (not working).

--- proof_of_concept/poc_e2e_pipeline.py
+++ proof_of_concept/poc_e2e_pipeline.py
@@ -1,13 +1,30 @@
 from src.coupon_selecting_alg import proto_coupons_from_frame, parse_proto_coupon
-from bert_inference.main import prepare_string_list, prepare_csv, map_strings_back_to_csv
+from bert_inference.main import prepare_csv, map_strings_back_to_csv
 from transformers import pipeline
+from src.constants import *
+import json
 
 csv_path = 'data/ner_data.csv'
 model_checkpoint = 'zpp-murmuras/bert_multiling_cased_test_data_test_1'
+
+labels_mapping = {
+    'PRICE': Label.PRICE,
+    'PRODUCT': Label.PRODUCT_NAME,
+    'DISCOUNT_PERCENTAGE': Label.PERCENT,
+    'DATE': Label.DATE,
+    'QUANTITY': Label.UNKNOWN,
+    'N/A': Label.UNKNOWN
+}
 
 bert_input = prepare_csv(csv_path)
 
 token_classifier = pipeline(
     "token-classification", model=model_checkpoint, aggregation_strategy="simple")
 
-map_strings_back_to_csv(bert_input, csv_path, token_classifier)
+df = map_strings_back_to_csv(bert_input, csv_path, token_classifier)
+
+results = []
+for pc in proto_coupons_from_frame(df, 'ner_tags', labels_mapping=labels_mapping):
+    results.append(parse_proto_coupon(pc))
+
+print(json.dumps(results, indent=4))
