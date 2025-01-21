f7768635dcd33f5de485185d8456be7b669f68a2
Gustaw Blachowski <gustaw.blachowski@gmail.com>
2025-01-21 22:00:01 +0100

    Init commit.

---
+++ llama_finetuning/input_parser.py
@@ -0,0 +1,43 @@
+import string
+
+import pandas as pd
+
+X_COLUMN = 'text'
+Y_COLUMN = 'seen_timestamp'
+
+def concat_column_x_by_column_y(column_x, column_y, df):
+    prev_col_val = None
+    result = {}
+    sub_result = ''
+    time = ''
+    for index, row in df.iterrows():
+        if row[column_y] == prev_col_val:
+            if str(row[column_x]) is not None:
+                sub_result += ' ' + str(row[column_x])
+        else:
+            if sub_result != '':
+                result[time] = sub_result
+            sub_result = str(row[column_x])
+            prev_col_val = row[column_y]
+            time = row['time']
+
+    return result
+
+def prepare_input_data(path: string):
+    data = pd.read_csv(path)
+    data.dropna(subset=['text'], inplace=True)
+    data_concat = concat_column_x_by_column_y(X_COLUMN, Y_COLUMN, data)
+    return data_concat
+
+def create_training_df(input_dict, gtd_dict):
+    concatenated_dfs = []
+    for key, value in input_dict.items():
+        if key in gtd_dict:
+            for item in gtd_dict[key]:
+                res = {'input': value, 'output': item}
+                concatenated_dfs.append(res)
+        else:
+            res = {'input': value, 'output': ''}
+            concatenated_dfs.append(res)
+
+    return pd.DataFrame(concatenated_dfs)
---
+++ llama_finetuning/main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+OPEN_API_KEY = 'TODO:API_KEY'
+
+if __name__ == '__main__':
+    lidl_content_generic = pd.read_csv('TODO:PATH')
+    lidl_coupons = pd.read_csv('TODO:PATH')
+
+
