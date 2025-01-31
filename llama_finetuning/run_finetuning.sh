0299531b7f6bb3aa616b9cb0e1d7a7f521606fea
Gustaw Blachowski <gustaw.blachowski@gmail.com>
2025-01-31 22:24:41 +0100

    Add script to run the finetuning.

---
+++ llama_finetuning/run_finetuning.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+declare -a NAMES=("one_input_one_output_wrequest" "one_input_one_output_wthrequest"
+                 "one_input_multiple_outputs_wrequest" "one_input_multiple_outputs_wthrequest")
+
+export HUGGING_FACE_TOKEN=
+export WANDB_KEY=
+
+for NAME in "${NAMES[@]}"
+do
+  export DATASET_NAME="$NAME"
+  modal run fine_tune_llama.py
+done
+
+unset HUGGING_FACE_TOKEN
+unset WANDB_KEY
+unset DATASET_NAME
