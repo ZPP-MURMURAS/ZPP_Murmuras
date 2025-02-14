8d6f4d51c429fc3662a1c25ad3efa83c38ad64f9
SzymonKozl <szymon.kozlowski.2003@gmail.com>
2025-02-14 18:18:14 +0100

    fixed ds generation script

--- tools/generate_llama_dataset_generic.py
+++ tools/generate_llama_dataset_generic.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import huggingface_hub
 import sys
+
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(parent_dir)
 import src.llama_dataset_generation.input_parser as ip
@@ -11,6 +12,7 @@ from transformers import AutoTokenizer
 import argparse
 
 HF_API_KEY = os.getenv('HF_API_KEY')
+OPEN_API_KEY = os.getenv('OPEN_API_KEY')
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Process input parameters.")
@@ -21,6 +23,7 @@ def parse_args():
         "Map function to use", choices=[f.__name__ for f in dt.MAP_FUNCTIONS]
     )
     parser.add_argument("--hf_name", required=True, help="Name on hf to save dataset under")
+    parser.add_argument("--no_ai", required=False, help="Whether to generate new type of dataset without chatgpt.", action="store_true")
 
     return parser.parse_args()
 
@@ -38,8 +41,11 @@ if __name__ == '__main__':
     assert MAP_FUN is not None
 
     HF_NAME = args.hf_name
+    no_ai = args.no_ai
 
-    print(HF_API_KEY)
+    client = None
+    if not no_ai:
+        client = gtp.init_client(HF_API_KEY)
     huggingface_hub.login(HF_API_KEY)
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
@@ -53,13 +59,15 @@ if __name__ == '__main__':
         discount_list = gtp.load_coupons(coupons_path)
         coupons_frame = pd.read_csv(coupons_path)
         cg_concat = ip.prepare_input_data(content_path)
+        if not no_ai:
+            # This will perform to OpenAI API, and overwrite ground_truth.json.
+            gtd = gtp.extract_discounts(discount_list, client)
 
-        # This will perform to OpenAI API, and overwrite ground_truth.json.
-        gtd = gtp.extract_discounts(discount_list, client)
-
-        # Said json is produced by the gtp.extract_discounts call and stored under
-        # the GROUND_TRUTH_JSON_PATH.
-        gtd_dict = gtp.prepare_ground_truth_data(gtd, coupons_frame)
+            # Said json is produced by the gtp.extract_discounts call and stored under
+            # the GROUND_TRUTH_JSON_PATH.
+            gtd_dict = gtp.prepare_ground_truth_data(gtd, coupons_frame)
+        else:
+            gtd_dict = gtp.prepare_ground_truth_data_no_ai(coupons_frame)
         for k in gtd_dict.keys():
             gtd_combined[f"{i}_{k}"] = gtd_dict[k]
         for k in cg_concat.keys():
@@ -70,4 +78,5 @@ if __name__ == '__main__':
     # Global var of the dt lib.
     dt.EOS_TOKEN = tokenizer.eos_token
     training_data = dt.run_mapping(training_df, MAP_FUN)
+    training_data = training_data.train_test_split(0.1)
     training_data.push_to_hub('zpp-murmuras/' + HF_NAME, private=True)
