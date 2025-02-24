e04af14c31f95a0d8699e0ad9c707d1a07c028c4
SzymonKozl <szymon.kozlowski.2003@gmail.com>
2025-02-24 09:32:46 +0100

    updated research conclusions

--- llama_finetuning/fine_tune_llama.py
+++ llama_finetuning/fine_tune_llama.py
@@ -19,15 +19,12 @@ finetune_image = (
           "TIMEOUT": os.getenv('TIMEOUT')})
 )
 
-def load_model(model_name, max_seq_length, wandb_key, name):
+def load_model(model_name, max_seq_length, wandb_key, name, wandb_project):
     from unsloth import FastLanguageModel
     import wandb
-    import os
 
-    print(wandb_key)
     wandb.login(key=wandb_key)
-    os.environ["WANDB_PROJECT"] = "CompareDataForLLamas"
-    os.environ["WANDB_RUN_ID"] = name
+    wandb.init(project=wandb_project, name=name)
 
     model, tokenizer = FastLanguageModel.from_pretrained(
         model_name=model_name,
@@ -50,7 +47,7 @@ def load_model(model_name, max_seq_length, wandb_key, name):
 
     return model, tokenizer
 
-def train_model(model, tokenizer, dataset_name, training_data, max_seq_length):
+def train_model(model, tokenizer, run_name, training_data, eval_data, max_seq_length, epoch_no):
     from trl import SFTTrainer
     from transformers import TrainingArguments
     from unsloth import is_bfloat16_supported
@@ -75,44 +72,55 @@ def train_model(model, tokenizer, dataset_name, training_data, max_seq_length):
         model=model,
         tokenizer=tokenizer,
         train_dataset=training_data,
+        eval_dataset=eval_data,
         dataset_text_field="text",
         #data_collator=collator,
         max_seq_length=max_seq_length,
         dataset_num_proc=2,
         packing=True, # True doesn't work with the current collator, BUT is faster.
         args=TrainingArguments(
-            learning_rate=3e-2,
+            learning_rate=3e-4,
             lr_scheduler_type="linear",
             per_device_train_batch_size=16,
             gradient_accumulation_steps=8,
-            num_train_epochs=25,
+            num_train_epochs=epoch_no,
             fp16=not is_bfloat16_supported(),
             bf16=is_bfloat16_supported(),
             logging_steps=1,
-            optim="adamw_8bit",
+            optim="lion_8bit",
             weight_decay=0.01,
             warmup_steps=10,
-            output_dir=dataset_name,
+            output_dir=run_name,
             seed=0,
-            report_to="wandb"
+            report_to="wandb",
+            eval_strategy="epoch",
+            logging_strategy="epoch"
         ),
     )
 
     trainer.train()
 
 @app.function(image=finetune_image, gpu="H100", timeout=int(os.getenv('TIMEOUT')))
-def wrapper(model_name, hf_token, wandb_key, dataset_name):
+def wrapper(model_name, hf_token, wandb_key, dataset_name, wandb_proj, epoch_no):
+    run_name = "example series lion_8bit"
     from datasets import load_dataset
 
     max_seq_length = 4096
-    model, tokenizer = load_model(model_name, max_seq_length, wandb_key, dataset_name)
+    model, tokenizer = load_model(model_name, max_seq_length, wandb_key, run_name, wandb_proj)
     training_data = load_dataset('zpp-murmuras/' + dataset_name, token=hf_token, split='train')
-    train_model(model, tokenizer, dataset_name, training_data, max_seq_length)
+    eval_data = load_dataset('zpp-murmuras/' + dataset_name, split='test')
+    print(len(training_data))
+    train_model(model, tokenizer, dataset_name, training_data, eval_data, max_seq_length, epoch_no)
 
 
 @app.local_entrypoint()
 def main():
-    hf_token = os.getenv('HUGGING_FACE_TOKEN')
-    wandb_key = os.getenv('WANDB_KEY')
-    dataset_name = os.getenv('DATASET_NAME')
-    wrapper.remote('meta-llama/Llama-3.2-1B', hf_token, wandb_key, dataset_name)
+    assert (hf_token := os.getenv('HUGGING_FACE_TOKEN')) is not None
+    assert (wandb_key := os.getenv('WANDB_KEY')) is not None
+    assert (dataset_name := os.getenv('DATASET_NAME')) is not None
+    assert (wandb_proj := os.getenv('WANDB_PROJECT')) is not None
+    try:
+        epoch_no = int(os.getenv('EPOCH_NO'))
+    except (ValueError, TypeError):
+        epoch_no = 25
+    wrapper.remote('meta-llama/Llama-3.2-1B', hf_token, wandb_key, dataset_name, wandb_proj, epoch_no)
