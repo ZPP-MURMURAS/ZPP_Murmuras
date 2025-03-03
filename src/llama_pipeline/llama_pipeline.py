fab6cbb2b0d4e5fcdf5579b04b0af86f01b90fbc
Kamil <kam.dyb1@gmail.com>
2025-03-03 23:36:36 +0100

    started work on llama pipeline

--- src/llama_pipeline/llama_pipeline.py
+++ src/llama_pipeline/llama_pipeline.py
@@ -1,16 +1,26 @@
 import sys
-from dotenv import load_dotenv
-from huggingface_hub import login, hf_hub_download
-from datasets import load_dataset
+import os
+from io import StringIO
 from llama_cpp import Llama
+
+CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.abspath(os.path.join(CURRENT_PATH, "../../")))
+
+from src.llama_dataset_generation.input_parser import prepare_input_data
 
 
 if __name__ == "__main__":
+    args = sys.argv
 
+    if len(args) != 2:
+        print("Usage: python llama_pipeline.py <model_path>")
+        sys.exit(1)
+
+    model_path = args[1]
+    input_csv_str = sys.stdin.read()
     llama = Llama(model_path)
+    input_data = prepare_input_data(StringIO(input_csv_str))
+    for context in input_data:
+        output = llama(context, max_tokens=512)
+        print(output["choices"][0]["text"])
 
-    # Run the model on the dataset
-    results = llama(dataset["test"]["text"])
-
-    # Print the results
-    print(results)
