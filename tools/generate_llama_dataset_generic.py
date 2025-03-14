75db22bf18727ddfa6ccf36fbadda9017a7bdbd1
GustawB <gustaw.blachowski@gmail.com>
2025-03-14 21:29:59 +0100

    Finish pipeline. Whats left is to change the format.

--- tools/generate_llama_dataset_generic.py
+++ tools/generate_llama_dataset_generic.py
@@ -2,6 +2,9 @@ import os
 import pandas as pd
 import sys
 import json
+
+from datasets import DatasetDict
+
 import src.llama_dataset_generation.input_parser as ip
 import src.llama_dataset_generation.ground_truth_parser as gtp
 import src.llama_dataset_generation.datasetter as dt
@@ -66,7 +69,12 @@ def traverse_files(data_dir: str):
     if content_path is not None and coupons_path is not None:
         new_dts = create_dataset(content_path, coupons_path)
         dts_list.append(new_dts)
-        dts_names_list.append(os.path.dirname(content_path))
+        if os.name =='nt':
+            dts_names_list.append(content_path.split("\\")[-2])
+        elif os.name == 'posix':
+            dts_names_list.append(content_path.split("/")[-2])
+        else:
+            raise Exception("Unsupported OS")
     return dts_list, dts_names_list
 
 
@@ -84,35 +92,6 @@ if __name__ == '__main__':
         client = gtp.init_client(OPEN_API_KEY)
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, token=HF_API_KEY)
 
-    traverse_files(DATA_PATH)
-
-    # cg_concat_combined = {}
-    # gtd_combined = {}
-    # for i, (file_content, file_coupons) in enumerate(zip(os.listdir(os.fsencode(INPUT_PATH)),
-    #                                       os.listdir(os.fsencode(GROUND_TRUTH_PATH)))):
-    #     coupons_path = os.path.join(GROUND_TRUTH_PATH, os.fsdecode(file_coupons))
-    #     content_path = os.path.join(INPUT_PATH, os.fsdecode(file_content))
-    #     discount_list = gtp.load_coupons(coupons_path)
-    #     coupons_frame = pd.read_csv(coupons_path)
-    #     cg_concat = ip.prepare_input_data(content_path)
-    #     if not no_ai:
-    #         # This will perform to OpenAI API, and overwrite ground_truth.json.
-    #         gtd = json.loads(gtp.extract_discounts(discount_list, client))
-    #
-    #         # Said json is produced by the gtp.extract_discounts call and stored under
-    #         # the GROUND_TRUTH_JSON_PATH.
-    #         gtd_dict = gtp.prepare_ground_truth_data(gtd, coupons_frame)
-    #     else:
-    #         gtd_dict = gtp.prepare_ground_truth_data_no_ai(coupons_frame)
-    #     for k in gtd_dict.keys():
-    #         gtd_combined[f"{i}_{k}"] = gtd_dict[k]
-    #     for k in cg_concat.keys():
-    #         cg_concat_combined[f"{i}_{k}"] = cg_concat[k]
-    #
-    # training_df = ip.create_training_df(cg_concat_combined, gtd_combined)
-    #
-    # # Global var of the dt lib.
-    # dt.EOS_TOKEN = tokenizer.eos_token
-    # training_data = dt.run_mapping(training_df, MAP_FUN)
-    # training_data = training_data.train_test_split(0.1)
-    # training_data.push_to_hub('zpp-murmuras/' + HF_NAME, private=True)
+    res_dts, res_dts_names = traverse_files(DATA_PATH)
+    merged_dts = DatasetDict(dict(zip(res_dts_names, res_dts)))
+    merged_dts.push_to_hub('zpp-murmuras/' + HF_NAME, private=True, token=HF_API_KEY)
