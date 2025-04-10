00b11a29486e45b3f13e4f5c82186ef01c3c7388
SzymonKozl <szymon.kozlowski.2003@gmail.com>
2025-04-10 08:08:11 +0200

    updated bert's modal script and requirements.txt

--- src/bert_finetuning/modal_script.py
+++ src/bert_finetuning/modal_script.py
@@ -6,6 +6,7 @@ import finetuner as ft
 import os
 
 __IGNORED_DS_SOURCES = ['penny', 'edeka']
+INCREMENT_SIZE = 100
 
 app = modal.App("BERT-fine-tuning")
 MODEL_CHECKPOINT = "google-bert/bert-base-multilingual-cased"
@@ -28,9 +29,9 @@ finetune_image = (
 
 def push_model_to_hub(model: callable, model_name: str, hf_token: str) -> None:
     model_repo = 'zpp-murmuras/' + model_name
-    model.push_to_hub(model_repo, token=hf_token)
+    model.push_to_hub(model_repo, token=hf_token, private=True)
     tokenizer = AutoTokenizer.from_pretrained(MODEL_CHECKPOINT)
-    tokenizer.push_to_hub(model_repo, token=hf_token)
+    tokenizer.push_to_hub(model_repo, token=hf_token, private=True)
 
 
 def define_name(base: str, dataset_name: str, app: str) -> str:
@@ -116,6 +117,7 @@ def run_fine_tuning_per_app(hf_token, wandb_key, dataset_name, push_to_hub=False
 
 @app.function(image=finetune_image, gpu="H100", timeout=60000)
 def run_fine_tuning_add_solo(hf_token, wandb_key, dataset_name, push_to_hub=False):
+    from random import sample
     cs = load_dataset('zpp-murmuras/' + dataset_name, token=hf_token)
     wandb.login(key=wandb_key)
     ft.init_finetuner(MODEL_CHECKPOINT)
@@ -131,21 +133,24 @@ def run_fine_tuning_add_solo(hf_token, wandb_key, dataset_name, push_to_hub=Fals
             train_split = int(len(ds) * 0.8)
             train_texts = ds['texts'][:train_split]
             train_labels = ds['labels'][:train_split]
+            len_train = len(train_texts)
 
             train_test_dataset = DatasetDict({
-                'train': Dataset.from_dict({'texts': train_texts, 'labels': train_labels}, features=ds.features),
+                'train': Dataset.from_dict({'texts': train_texts, 'labels': train_labels}, features=ds.features)
+                .select(sample(range(len_train), min(INCREMENT_SIZE, len_train))),
                 'test': Dataset.from_dict({'texts': test_texts, 'labels': test_labels}, features=ds.features)
             })
 
-            model_name = define_name('bert-selection-add-solo', dataset_name, ds_name)
+            model_name = define_name('bert-extraction-add-solo-2', dataset_name, ds_name)
             #model_name = 'bert_extraction_add_solo' + '-' + ds_name
-            ft.train_model(model, train_test_dataset, labels, wandb_log='bert_selection_add_solo_ft', run_name=model_name, curriculum_learning=False, splits=10)
+            ft.train_model(model, train_test_dataset, labels, wandb_log='bert_extraction_add_solo_ft-2', run_name=model_name, curriculum_learning=False, splits=10)
             if push_to_hub:
                 push_model_to_hub(model, model_name, hf_token)
 
 
 @app.function(image=finetune_image, gpu="H100", timeout=60000)
 def run_fine_tuning_add_grow(hf_token, wandb_key, dataset_name, push_to_hub=False):
+    from random import sample
     cs = load_dataset('zpp-murmuras/' + dataset_name, token=hf_token)
     wandb.login(key=wandb_key)
     ft.init_finetuner(MODEL_CHECKPOINT)
@@ -161,15 +166,16 @@ def run_fine_tuning_add_grow(hf_token, wandb_key, dataset_name, push_to_hub=Fals
         if ds_name not in __IGNORED_DS_SOURCES:
             ds = cs[ds_name]
             train_split = int(len(ds) * 0.8)
+            train_indices = sample(range(train_split), min(INCREMENT_SIZE, train_split))
             train_texts = ds['texts'][:train_split]
             train_labels = ds['labels'][:train_split]
 
-            total_dataset['train'] = concatenate_datasets([total_dataset['train'], Dataset.from_dict({'texts': train_texts, 'labels': train_labels}, features=ds.features)])
+            total_dataset['train'] = concatenate_datasets([total_dataset['train'], Dataset.from_dict({'texts': train_texts, 'labels': train_labels}, features=ds.features).select(train_indices)])
             total_dataset['train'] = total_dataset['train'].shuffle(seed=42)
 
-            model_name = define_name('bert-selection-add-grow', dataset_name, ds_name)
+            model_name = define_name('bert-extraction-add-grow-2', dataset_name, ds_name)
             #model_name = 'bert_extraction_add_grow' + '-' + ds_name
-            ft.train_model(model, total_dataset, labels, wandb_log='bert_selection_add_grow_ft', run_name=model_name, curriculum_learning=False, splits=10)
+            ft.train_model(model, total_dataset, labels, wandb_log='bert_extraction_add_grow_ft-2', run_name=model_name, curriculum_learning=False, splits=10)
             if push_to_hub:
                 push_model_to_hub(model, model_name, hf_token)
 
@@ -180,6 +186,6 @@ def main():
     wandb_key = os.getenv('WANDB_KEY')
     dataset_name = os.getenv('DATASET_NAME')
     #run_fine_tuning.remote(hf_token, wandb_key, dataset_name, True)
-    run_fine_tuning_per_app.remote(hf_token, wandb_key, dataset_name, True)
+    #run_fine_tuning_per_app.remote(hf_token, wandb_key, dataset_name, True)
     run_fine_tuning_add_solo.remote(hf_token, wandb_key, dataset_name, True)
     run_fine_tuning_add_grow.remote(hf_token, wandb_key, dataset_name, True)
